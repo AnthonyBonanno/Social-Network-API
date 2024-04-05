2b937a4958b6e953399878c71a8ff2272dd66374
AnthonyBonanno <bonant12@gmail.com>
2024-04-06 01:52:23 +1100

    added some models, and other basic files

---
+++ models/Thoughts.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+const reactsSchema = require("./Reacts");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (currentDate) => {
+        return currentDate;
+      },
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    // this array will be the entire schema for the react (id, react type, ect...)
+    reactions: [reactsSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("thoughts", thoughtSchema);
+
+module.exports = Thoughts;
